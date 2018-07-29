3c068f3e34cc4a5edd3453c3aa1251225fd2934e
Ken Truong <kenvantruong@gmail.com>
2018-07-28 20:34:18 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,27 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './components/common/Header';
-import List from './components/list/List';
 import './index.css';
+import App from './components/App';
 
-
-
-const App = () => {
-  const title = 'React Coin';
-
-  return (
-  <div>
-    <Header />
-    <List />
-
-
-          
-    
-  </div>
-  )
-}
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
